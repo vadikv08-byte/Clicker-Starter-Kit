5b3a0f9c408cf17f1dea587f1a6f22804f4331d9
vadikv08-byte <vadik_v08@ukr.net>
2026-04-27 21:48:44 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -1,19 +1,11 @@
-const { Bot } = require("grammy");
+const { Bot, webhookCallback } = require("grammy");
 
-// Твой токен, который ты получил от BotFather
+// Твой токен нового бота
 const bot = new Bot("8382077398:AAGy-ftFiO_t3_s0RslBbB5bcLSINftCFmo");
 
 bot.command("start", (ctx) => 
-  ctx.reply("Здравствуйте! Я — Хранитель памяти. Нажмите кнопку ниже, чтобы открыть ваш личный архив.")
+  ctx.reply("Здравствуйте! Я — Хранитель памяти. Нажмите кнопку меню, чтобы открыть ваш личный архив.")
 );
 
-export default async function handler(request, response) {
-  try {
-    if (request.method === "POST") {
-      await bot.handleUpdate(request.body);
-    }
-    response.status(200).send("OK");
-  } catch (e) {
-    response.status(500).send("Error");
-  }
-}
+// Это специальный формат для работы внутри Vercel
+module.exports = webhookCallback(bot, "nextjs");
