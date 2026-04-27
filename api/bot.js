9a0d1430c0cea39d23658eb5175d7aaf88125d98
vadikv08-byte <vadik_v08@ukr.net>
2026-04-27 21:54:08 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -1,11 +1,9 @@
 const { Bot, webhookCallback } = require("grammy");
 
-// Токен твоего бота Store_Memory_bot
 const bot = new Bot("8382077398:AAGy-ftFiO_t3_s0RslBbB5bcLSINftCFmo");
 
 bot.command("start", (ctx) => 
-  ctx.reply("Здравствуйте! Я — Хранитель памяти. Нажмите кнопку меню, чтобы открыть ваш личный архив.")
+  ctx.reply("Здравствуйте! Я — Хранитель памяти. Нажмите кнопку внизу, чтобы открыть архив.")
 );
 
-// Экспорт для Vercel
 module.exports = webhookCallback(bot, "nextjs");
